35831729da8f5ce36cfc2bd0c618fbae5b35e73b
zooniii <130193522+zooniii@users.noreply.github.com>
2023-08-31 14:28:44 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://sazi.online/graphql"
+	const endpoint = "https://scooparticle.online/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://sazi.online/' + encodeURI(path as string)
+					'https://scooparticle.online/' + encodeURI(path as string)
 				}`,
 			},
 		};
